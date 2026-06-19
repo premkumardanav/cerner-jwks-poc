a893ce03962b735f657e0cae847af8aecea54f78
Premkumar Danav <premkumar.danav@mindbowser.com>
2026-06-19 11:25:22 +0530

    feat: add routes for common clinical and reference FHIR resources

--- src/routes/fhir.routes.js
+++ src/routes/fhir.routes.js
@@ -3,6 +3,22 @@ import {
   getPatientById,
   searchObservationsByPatient,
   searchPatients,
+  searchAllergiesByPatient,
+  searchConditionsByPatient,
+  searchMedicationRequestsByPatient,
+  searchEncountersByPatient,
+  getEncounterById,
+  searchProceduresByPatient,
+  searchImmunizationsByPatient,
+  searchDiagnosticReportsByPatient,
+  searchDocumentReferencesByPatient,
+  searchCarePlansByPatient,
+  searchCareTeamsByPatient,
+  searchGoalsByPatient,
+  searchCoverageByPatient,
+  getPractitionerById,
+  getOrganizationById,
+  getLocationById,
 } from "../services/fhir.service.js";
 
 const router = Router();
@@ -25,6 +41,114 @@ router.get("/patient/:id/observations", async (req, res, next) => {
   }
 });
 
+router.get("/patient/:id/allergies", async (req, res, next) => {
+  try {
+    const data = await searchAllergiesByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/conditions", async (req, res, next) => {
+  try {
+    const data = await searchConditionsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/medications", async (req, res, next) => {
+  try {
+    const data = await searchMedicationRequestsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/encounters", async (req, res, next) => {
+  try {
+    const data = await searchEncountersByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/procedures", async (req, res, next) => {
+  try {
+    const data = await searchProceduresByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/immunizations", async (req, res, next) => {
+  try {
+    const data = await searchImmunizationsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/diagnostic-reports", async (req, res, next) => {
+  try {
+    const data = await searchDiagnosticReportsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/documents", async (req, res, next) => {
+  try {
+    const data = await searchDocumentReferencesByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/care-plans", async (req, res, next) => {
+  try {
+    const data = await searchCarePlansByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/care-teams", async (req, res, next) => {
+  try {
+    const data = await searchCareTeamsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/goals", async (req, res, next) => {
+  try {
+    const data = await searchGoalsByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/patient/:id/coverage", async (req, res, next) => {
+  try {
+    const data = await searchCoverageByPatient(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.get("/patient-search", async (req, res, next) => {
   try {
     const { family, given, birthdate, identifier, phone, email } = req.query;
@@ -38,6 +162,42 @@ router.get("/patient-search", async (req, res, next) => {
     if (email) params.email = email;
 
     const data = await searchPatients(params);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/encounter/:id", async (req, res, next) => {
+  try {
+    const data = await getEncounterById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/practitioner/:id", async (req, res, next) => {
+  try {
+    const data = await getPractitionerById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/organization/:id", async (req, res, next) => {
+  try {
+    const data = await getOrganizationById(req.params.id);
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/location/:id", async (req, res, next) => {
+  try {
+    const data = await getLocationById(req.params.id);
     res.json(data);
   } catch (error) {
     next(error);
--- src/services/fhir.service.js
+++ src/services/fhir.service.js
@@ -2,52 +2,92 @@ import axios from "axios";
 import { env } from "../config/env.js";
 import { getAccessToken } from "./token.service.js";
 
-export async function getPatientById(patientId) {
+async function fhirGet(path) {
   const token = await getAccessToken();
 
-  const { data } = await axios.get(
-    `${env.cernerFhirBaseUrl}/Patient/${patientId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/fhir+json",
-      },
+  const { data } = await axios.get(`${env.cernerFhirBaseUrl}${path}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/fhir+json",
     },
-  );
+  });
 
   return data;
 }
 
-export async function searchObservationsByPatient(patientId) {
-  const token = await getAccessToken();
-
-  const { data } = await axios.get(
-    `${env.cernerFhirBaseUrl}/Observation?patient=${patientId}`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/fhir+json",
-      },
-    },
-  );
-
-  return data;
+export async function getPatientById(patientId) {
+  return fhirGet(`/Patient/${patientId}`);
 }
 
 export async function searchPatients(params) {
-  const token = await getAccessToken();
-
   const query = new URLSearchParams(params).toString();
+  return fhirGet(`/Patient?${query}`);
+}
 
-  const { data } = await axios.get(
-    `${env.cernerFhirBaseUrl}/Patient?${query}`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/fhir+json",
-      },
-    },
-  );
+export async function searchObservationsByPatient(patientId) {
+  return fhirGet(`/Observation?patient=${patientId}`);
+}
 
-  return data;
+export async function searchAllergiesByPatient(patientId) {
+  return fhirGet(`/AllergyIntolerance?patient=${patientId}`);
+}
+
+export async function searchConditionsByPatient(patientId) {
+  return fhirGet(`/Condition?patient=${patientId}`);
+}
+
+export async function searchMedicationRequestsByPatient(patientId) {
+  return fhirGet(`/MedicationRequest?patient=${patientId}`);
+}
+
+export async function searchEncountersByPatient(patientId) {
+  return fhirGet(`/Encounter?patient=${patientId}`);
+}
+
+export async function getEncounterById(encounterId) {
+  return fhirGet(`/Encounter/${encounterId}`);
+}
+
+export async function searchProceduresByPatient(patientId) {
+  return fhirGet(`/Procedure?patient=${patientId}`);
+}
+
+export async function searchImmunizationsByPatient(patientId) {
+  return fhirGet(`/Immunization?patient=${patientId}`);
+}
+
+export async function searchDiagnosticReportsByPatient(patientId) {
+  return fhirGet(`/DiagnosticReport?patient=${patientId}`);
+}
+
+export async function searchDocumentReferencesByPatient(patientId) {
+  return fhirGet(`/DocumentReference?patient=${patientId}`);
+}
+
+export async function searchCarePlansByPatient(patientId) {
+  return fhirGet(`/CarePlan?patient=${patientId}`);
+}
+
+export async function searchCareTeamsByPatient(patientId) {
+  return fhirGet(`/CareTeam?patient=${patientId}`);
+}
+
+export async function searchGoalsByPatient(patientId) {
+  return fhirGet(`/Goal?patient=${patientId}`);
+}
+
+export async function searchCoverageByPatient(patientId) {
+  return fhirGet(`/Coverage?patient=${patientId}`);
+}
+
+export async function getPractitionerById(practitionerId) {
+  return fhirGet(`/Practitioner/${practitionerId}`);
+}
+
+export async function getOrganizationById(organizationId) {
+  return fhirGet(`/Organization/${organizationId}`);
+}
+
+export async function getLocationById(locationId) {
+  return fhirGet(`/Location/${locationId}`);
 }
